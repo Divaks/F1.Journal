289796e9d50751ddfbb4b2a4a81233a8737340ab
dmytro <dmitrovolicenko@gmail.com>
2025-11-19 03:17:24 +0200

    added migrations

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -72,6 +72,12 @@ builder.Services.AddAuthentication("Bearer")
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
